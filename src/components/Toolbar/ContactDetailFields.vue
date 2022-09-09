4701a0d9f8ba78a39d35d45e509e686cffd176c8
Leonardo <oliveirabalsa2@gmail.com>
2022-09-09 17:18:38 -0300

    toolbar index component

---
+++ src/components/Toolbar/ContactDetailFields.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-col class="pa-0">
+    <h5 class="font-weight-thin">{{ title }}</h5>
+    <p class="body-2 font-weight-bold">{{ value }}</p>
+  </v-col>
+</template>
+<script>
+export default {
+    props: {
+        title: String,
+        value: String
+    },
+  name: "ContactDetailFields",
+};
+</script>
---
+++ src/components/Toolbar/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-col class="tollbar" cols="4">
+    <ToolbarNavigator />
+    <ToolbarActions />
+    <v-divider></v-divider>
+    <ToolbarContactDetails />
+    <v-divider></v-divider>
+    <ToolbarAgreement />
+    <v-divider></v-divider>
+    <ToolbarButtonSection />
+    <v-divider></v-divider>
+    <ToolbarTeamSection />
+  </v-col>
+</template>
+<script>
+import ToolbarContactDetails from "./ToolbarContactDetails";
+import ToolbarNavigator from "./ToolbarNavigator";
+import ToolbarActions from "./ToolbarActions";
+import ToolbarAgreement from "./ToolbarAgreement";
+import ToolbarButtonSection from "./ToolbarButtonSection";
+import ToolbarTeamSection from "./ToolbarTeamSection";
+export default {
+  name: "ToolbarPage",
+  data: () => ({}),
+  components: {
+    ToolbarContactDetails,
+    ToolbarNavigator,
+    ToolbarActions,
+    ToolbarAgreement,
+    ToolbarButtonSection,
+    ToolbarTeamSection,
+  },
+};
+</script>
+<style scoped>
+.tollbar {
+  background-color: #fff;
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgba(173, 173, 173, 0.2);
+}
+</style>
