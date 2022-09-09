04c33d9285fc423790fc3e8c16a58ea222f1bd04
Leonardo <oliveirabalsa2@gmail.com>
2022-09-09 17:17:03 -0300

    feat: toolbar agreement component

---
+++ src/components/Toolbar/ToolbarAgreement.vue
@@ -0,0 +1,18 @@
+<template>
+            <v-row
+          class="mt-3 mb-3 px-2"
+          align-content="center"
+          justify="space-between"
+        >
+          <h4>
+            <v-icon color="primary">mdi-chevron-right</v-icon>
+            Agreement
+          </h4>
+        </v-row>
+</template>
+<script>
+export default {
+  name: "ToolbarAgreement",
+  data: () => ({}),
+};
+</script>
