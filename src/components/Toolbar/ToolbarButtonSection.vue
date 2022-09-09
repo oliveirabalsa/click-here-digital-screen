7d217dd8433f1d6aee63ad27b12415875aecf409
Leonardo <oliveirabalsa2@gmail.com>
2022-09-09 17:17:14 -0300

    feat: toolbar button section component

---
+++ src/components/Toolbar/ToolbarButtonSection.vue
@@ -0,0 +1,29 @@
+<template>
+            <v-row class="mt-2 mb-4">
+          <v-btn
+            v-for="(buttonName, index) of buttonNames"
+            :key="index"
+            color="primary"
+            small
+            min-width="110px"
+            max-width="160px"
+            class="ml-1 mt-2"
+            >{{ buttonName }}</v-btn
+          >
+        </v-row>
+</template>
+<script>
+export default {
+  name: "ToolbarButtonSection",
+  data: () => ({
+    buttonNames: [
+      "Budgets",
+      "Insights",
+      "Add Assignment",
+      "Website",
+      "Agreement",
+      "Add Assignment",
+    ],
+  }),
+};
+</script>
